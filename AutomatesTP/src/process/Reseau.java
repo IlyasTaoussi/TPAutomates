e629b502b875781caca55565df779320a3e621e9
LilianTantot <tantotlilian@gmail.com>
2021-03-19 11:38:32 +0100

    Update Reseau.java

--- AutomatesTP/src/process/Reseau.java
+++ AutomatesTP/src/process/Reseau.java
@@ -5,5 +5,24 @@ import java.util.ArrayList;
 public class Reseau {
 	private ArrayList<Sommet> ListSommet;
 	private ArrayList<Arc> ListArc;
-	private ArrayList<Horaire> ListHoraire;
+	
+	public Reseau(ArrayList<Sommet> listSommet, ArrayList<Arc> listArc, ArrayList<Horaire> listHoraire) {
+		super();
+		ListSommet = listSommet;
+		ListArc = listArc;
+	}
+	
+	public ArrayList<Sommet> getListSommet() {
+		return ListSommet;
+	}
+	public void setListSommet(ArrayList<Sommet> listSommet) {
+		ListSommet = listSommet;
+	}
+	public ArrayList<Arc> getListArc() {
+		return ListArc;
+	}
+	public void setListArc(ArrayList<Arc> listArc) {
+		ListArc = listArc;
+	}
+	
 }
