acfb6eef20724a90f7a2fecbe87cb4f30f88ab20
IlyasTaoussi <ilyas.taoussi2000@gmail.com>
2021-03-19 10:38:20 +0100

    Update Class

--- AutomatesTP/src/process/Arc.java
+++ AutomatesTP/src/process/Arc.java
@@ -1,8 +1,11 @@
 package process;
 
+import java.util.List;
+
 public class Arc {
 	private Sommet stationDepart;
 	private Sommet stationArrivée;
-	private Horaire horaire;
+	private List<Horaire> horaires;
+	
 	
 }
