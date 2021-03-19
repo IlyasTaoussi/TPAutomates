35430293426831d5614b5d5b692d38c96ad27652
IlyasTaoussi <ilyas.taoussi2000@gmail.com>
2021-03-19 11:01:25 +0100

    Update Sommet.java

--- AutomatesTP/src/process/Sommet.java
+++ AutomatesTP/src/process/Sommet.java
@@ -5,4 +5,37 @@ public class Sommet {
 	private String nomStation;
 	private ArrayList<Arc> trajets;
 	
+	public Sommet() {
+		super();
+	}
+	
+	public Sommet(String nomStation) {
+		super();
+		this.nomStation = nomStation;
+	}
+
+	public String getNomStation() {
+		return nomStation;
+	}
+
+	public void setNomStation(String nomStation) {
+		this.nomStation = nomStation;
+	}
+
+	public ArrayList<Arc> getTrajets() {
+		return trajets;
+	}
+
+	public void setTrajets(ArrayList<Arc> trajets) {
+		this.trajets = trajets;
+	}
+	
+	public void addTrajet(Arc trajet) {
+		if(trajet.getStationDepart().getNomStation().equals(this.nomStation))
+			trajets.add(trajet);
+		else
+			System.err.println("Station de Depart non valide !!!!!");
+	}
+	
+	
 }
