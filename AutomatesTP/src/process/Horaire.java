0efdf4a35c46cc6f72a7c6a010a0c8e21156dabf
LilianTantot <tantotlilian@gmail.com>
2021-03-19 11:02:01 +0100

    update horaire

--- AutomatesTP/src/process/Horaire.java
+++ AutomatesTP/src/process/Horaire.java
@@ -1,7 +1,34 @@
 package process;
 
 public class Horaire {
-	private Heure dateDepart;
-	private Heure dateArrivee;
+	private Heure heureDepart;
+	private Heure heureArrivee;
+	private Heure duree;
+	
+	public Horaire(Heure heureDepart, Heure heureArrivee, Heure duree) {
+		super();
+		this.heureDepart = heureDepart;
+		this.heureArrivee = heureArrivee;
+		this.duree = duree;
+	}
+	
+	public Heure getHeureDepart() {
+		return heureDepart;
+	}
+	public void setHeureDepart(Heure heureDepart) {
+		this.heureDepart = heureDepart;
+	}
+	public Heure getHeureArrivee() {
+		return heureArrivee;
+	}
+	public void setHeureArrivee(Heure heureArrivee) {
+		this.heureArrivee = heureArrivee;
+	}
+	public Heure getDuree() {
+		return duree;
+	}
+	public void setDuree(Heure duree) {
+		this.duree = duree;
+	}
 	
 }
