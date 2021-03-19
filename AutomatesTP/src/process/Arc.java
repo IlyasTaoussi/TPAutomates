e65b3fadcdf17f3eccf6817b5a13d490f7d90d43
IlyasTaoussi <ilyas.taoussi2000@gmail.com>
2021-03-19 11:01:15 +0100

    Update Arc.java

--- AutomatesTP/src/process/Arc.java
+++ AutomatesTP/src/process/Arc.java
@@ -7,4 +7,51 @@ public class Arc {
 	private Sommet stationArrivée;
 	private List<Horaire> horaires;
 	private Transport transport;
+	
+	public Arc() {
+		super();
+	}
+	
+	public Arc(Sommet stationDepart, Sommet stationArrivée, Transport transport) {
+		super();
+		this.stationDepart = stationDepart;
+		this.stationArrivée = stationArrivée;
+		this.transport = transport;
+	}
+
+	public Sommet getStationDepart() {
+		return stationDepart;
+	}
+
+	public void setStationDepart(Sommet stationDepart) {
+		this.stationDepart = stationDepart;
+	}
+
+	public Sommet getStationArrivée() {
+		return stationArrivée;
+	}
+
+	public void setStationArrivée(Sommet stationArrivée) {
+		this.stationArrivée = stationArrivée;
+	}
+
+	public List<Horaire> getHoraires() {
+		return horaires;
+	}
+
+	public void setHoraires(List<Horaire> horaires) {
+		this.horaires = horaires;
+	}
+
+	public Transport getTransport() {
+		return transport;
+	}
+
+	public void setTransport(Transport transport) {
+		this.transport = transport;
+	}
+	
+	public void addHoraire(Horaire horaire) {
+		
+	}
 }
