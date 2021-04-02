75228f74991454c93df507a23f5738a26355000c
IlyasTaoussi <ilyas.taoussi2000@gmail.com>
2021-04-02 10:30:58 +0200

    Update LectureXML.java

--- AutomatesTP/src/process/LectureXML.java
+++ AutomatesTP/src/process/LectureXML.java
@@ -18,6 +18,7 @@ public class LectureXML {
 		super();
 		this.path = path;
 		this.setDefaultHandler(exploitant);
+		
 	}
 	
 	public void lectureTram() {
@@ -32,6 +33,7 @@ public class LectureXML {
 	       e.printStackTrace();
 	     }
 	   }
+	
 	
 	public void setDefaultHandler(Transport exploitant) {
 		if(exploitant.equals(Transport.TRAIN)) {
